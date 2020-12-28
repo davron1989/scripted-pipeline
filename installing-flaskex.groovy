eeb4919a3df338077d9b518879fd621ec00c8f92
davron1989 <davron.n@yahoo.com>
2020-12-28 02:17:56 +0000

    updated flaskex file

--- installing-flaskex.groovy
+++ installing-flaskex.groovy
@@ -21,7 +21,7 @@ node {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} git clone https://github.com/anfederico/Flaskex "
         }
         stage("changing directory"){
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} cd Flaskex "
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} cd Flaskex/ "
         }
         stage("Installing requirements"){
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${params.SSHNODE} pip install -r requirements.txt "
